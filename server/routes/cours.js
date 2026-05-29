f26c2acd3a00931bfec00e66482a959dc7dcb799
BurriSimon <simon.burri@ceff.ch>
2026-05-29 10:45:51 +0200

    Update 10:45 29.05.2026

--- server/routes/cours.js
+++ server/routes/cours.js
@@ -1,11 +1,13 @@
 const Express = require("express");
 
+const is_auth = require("../middlewares/is-auth");
+
 const CoursController = require("../controllers/cours");
 
 const router = Express.Router();
 
-router.get('/', CoursController.get_index);
+router.get('/', is_auth, CoursController.get_index);
 
-router.post('/present', CoursController.put_present);
+router.post('/present', is_auth, CoursController.put_present);
 
 module.exports = router;
