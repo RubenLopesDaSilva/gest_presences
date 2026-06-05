9b97e71e41bb803a5b53ef656ac8c6d4972df078
RubenLopesDaSilva <ruben.lopesdasilva@ceff.ch>
2026-06-05 09:44:41 +0200

    Comment router.signup

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,18 +1,21 @@
 const Express = require("express");
 const authController = require("../controllers/auth")
+const User = require("../model/user")
 
 const router = Express.Router();
 
 router.get('/', authController.getAllUsers);
 
-router.post('/signup', [body('email').custom((value, { req }) => {
-    return UserActivation.findOne({ email: value }).then(userDoc => {
-        if (userDoc) {
-            return Promise.reject('E-mail address already exists !')
-        }
-    })
-}).normalizeEmail()], authController.signup)
+router.post('/signup', authController.signup)
 
 router.post('/login', authController.login)
+
+// [body('email').custom((value, { req }) => {
+//     return User.findOne({ email: value }).then(userDoc => {
+//         if (userDoc) {
+//             return Promise.reject('E-mail address already exists !')
+//         }
+//     })
+// }).normalizeEmail()],
 
 module.exports = router;
